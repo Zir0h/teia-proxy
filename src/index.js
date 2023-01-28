6f479b88e7ff6a63e0ea8201e9d8a7ae4e8d121d
Yannick Goossens <yannick@ziroh.be>
2023-01-28 15:53:54 +0100

    fetch some more stuff like profile pic in case of /tz/ link or artifact_uri in case display_uri is empty

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+async function getProfileDetails(profileAddress) {
+  const response = await fetch(HICDEX_API, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: `
+        query GetProfileInfos {
+            holder: holder_by_pk(address: ${profileAddress}) {
+                metadata(path: "identicon")
+                address
+                name
+                description
+            }
+        }
+      `,
+    }),
+  });
+
+  const json = await response.json();
+  return json && json.data && json.data.holder;
+}
+
 async function getTokenDetails(tokenId) {
   const response = await fetch(HICDEX_API, {
     method: 'POST',
@@ -13,6 +38,7 @@ async function getTokenDetails(tokenId) {
                 title
                 description
                 display_uri
+                artifact_uri
             }
         }
       `,
@@ -27,30 +53,31 @@ function clean(str) {
   return str.replaceAll('"', '');
 }
 
-function injectOpenGraphTags(body, token, originalUrl) {
+function injectOpenGraphTags(body, info, originalUrl) {
   let newBody = body;
 
   // remove existing og tags
   newBody = newBody.replace(/<meta.*?property="og.*?\/>/gm, '');
   newBody = newBody.replace(/<meta.*?name="twitter.*?\/>/gm, '');
 
-  const title = clean(token.title);
-  const description = clean(token.description);
-  const image = clean(token.display_uri.replace('ipfs://', IPFS_GATEWAY));
-  const url = `${originalUrl.protocol}//${originalUrl.hostname}/objkt/${token.id}`;
+  const title = clean(`${info.title ? info.title : info.name}`);
+  const description = clean(info.description);
+  const image = clean(`${info.display_uri ? info.display_uri : info.metadata}`.replace('ipfs://', IPFS_GATEWAY));
+  const image_backup = clean(`${info.artifact_uri ? info.artifact_uri : ""}`.replace('ipfs://', IPFS_GATEWAY));
+  const url = `${originalUrl.protocol}//${originalUrl.hostname}/${info.title ? "objkt" : "tz"}/${info.id ? info.id : info.address}`;
 
   const openGraphTags = `
     <meta property="og:type" content="website" />
     <meta property="og:title" content="${title}" />
     <meta property="og:description" content="${description}" />
-    <meta property="og:image" content="${image}" />
+    <meta property="og:image" content="${image ? image : image_backup}" />
     <meta property="og:url" content="${url}" />
 
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:creator" content="@TeiaCommunity" />
     <meta name="twitter:title" content="${title}" />
     <meta name="twitter:description" content="${description}" />
-    <meta name="twitter:image" content="${image}" />
+    <meta name="twitter:image" content="${image ? image : image_backup}" />
   `;
 
   return newBody.replace('<head>', `<head>${openGraphTags}`);
@@ -66,22 +93,40 @@ async function handleRequest(request) {
   url.hostname = TARGET_HOSTNAME;
 
   try {
-    const detailPageMatch = url.pathname.match(/\/objkt\/([0-9]+)/);
+    const profilePageMatch = url.pathname.match(/\/tz\/(tz[a-zA-Z0-9]{34})/);
 
-    if (detailPageMatch) {
-      const tokenId = detailPageMatch[1];
+    if (profilePageMatch) {
+      const address = profilePageMatch[1];
 
       // TODO: set the correct headers.
       const teiaRequest = new Request(request, { headers: { 'Cache-Control': 'no-cache' } });
-      const [response, token] = await Promise.all([fetch(url.toString(), teiaRequest), getTokenDetails(tokenId)]);
+      const [response, profile] = await Promise.all([fetch(url.toString(), teiaRequest), getProfileDetails(address)]);
 
-      if (!token) {
-        throw new Error(`could not fetch token ${tokenId}`);
+      if (!profile) {
+        throw new Error(`could not fetch profile information ${address}`);
       }
 
       const body = await response.text();
 
-      return new Response(injectOpenGraphTags(body, token, new URL(request.url)), response);
+      return new Response(injectOpenGraphTags(body, profile, new URL(request.url)), response);
+    } else {
+      const detailPageMatch = url.pathname.match(/\/objkt\/([0-9]+)/);
+
+      if (detailPageMatch) {
+        const tokenId = detailPageMatch[1];
+
+        // TODO: set the correct headers.
+        const teiaRequest = new Request(request, { headers: { 'Cache-Control': 'no-cache' } });
+        const [response, token] = await Promise.all([fetch(url.toString(), teiaRequest), getTokenDetails(tokenId)]);
+
+        if (!token) {
+          throw new Error(`could not fetch token ${tokenId}`);
+        }
+
+        const body = await response.text();
+
+        return new Response(injectOpenGraphTags(body, token, new URL(request.url)), response);
+      }
     }
   } catch (err) {
     console.log('failed to process token metadata', err);
